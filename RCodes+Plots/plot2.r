91c0f9263580e0d8470c10463a34608f4877db7a
Yanal Kashou <yanal.kashou@gmail.com>
2016-03-12 13:33:36 +0200

    Update plot2.r

--- RCodes+Plots/plot2.r
+++ RCodes+Plots/plot2.r
@@ -11,6 +11,7 @@ if(!file.exists("household_power_consumption.txt")) {
 dt <- read.table("C:/Users/Yanal/Documents/household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", stringsAsFactors = FALSE)
 dt_date_subset <- dt[dt$Date %in% c("1/2/2007","2/2/2007") ,]
 date_time <- strptime(paste(dt_date_subset$Date, dt_date_subset$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+
 globalActivePower <- as.numeric(dt_date_subset$Global_active_power)
 
 # Plotting ~ plot2.png
