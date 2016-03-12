cd6e01256ba1dfbc3a2dfccf21fda0a97d6f6b7a
Yanal Kashou <yanal.kashou@gmail.com>
2016-03-12 13:36:39 +0200

    Update plot1.r

--- RCodes+Plots/plot1.r
+++ RCodes+Plots/plot1.r
@@ -7,16 +7,21 @@ if(!file.exists("household_power_consumption.txt")) {
         unzip(f)
 }
 
-# Load and Explore dataset
+# Load and Explore dataset,
 dt <- read.table("C:/Users/Yanal/Documents/household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", stringsAsFactors = FALSE)
 
 dim(dt)
 head(dt)
 summary(dt)
 
+# Subset dates 1/2/2007 and 2/2/2007
 dt_date_subset <- dt[dt$Date %in% c("1/2/2007","2/2/2007") ,]
 str(dt_date_subset)
+
+# Associated Variables
 globalActivePower <- as.numeric(dt_date_subset$Global_active_power)
+
+# Plotting ~ plot2.png
 png("plot1.png", width = 480, height = 480)
 hist(globalActivePower, col = "red", main = "Global Active Power", xlab = "Global Active Power(kilowatts)", ylab = "Frequency")
 dev.off()
